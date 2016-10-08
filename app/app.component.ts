e95e07db73366a498117c394a4ea3af1edbd22cb
xiaofuyesnew <xiaofuyesnew@qq.com>
2016-10-08 21:20:07 +0800

    双向绑定

--- app/app.component.ts
+++ app/app.component.ts
@@ -12,7 +12,10 @@ export class Hero{
         <h1>{{title}}</h1>
         <h2>{{hero.name}} details!</h2>
         <div><label>id: </label>{{hero.id}}</div>
-        <div><label>name: </label>{{hero.name}}</div>
+        <div>
+            <label>name: </label>
+            <input type="text" [(ngModel)]="hero.name" placeholder="name">
+        </div>
     `
 })
 
