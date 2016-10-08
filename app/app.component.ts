6b614bd486afda70381424bc315ac62217cc865a
xiaofuyesnew <xiaofuyesnew@qq.com>
2016-10-08 18:23:40 +0800

    change

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,25 @@
 import { Component } from '@angular/core';
+//Hero类
+export class Hero{
+    id : number;
+    name : string;
+}
+
+//装饰器
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular App</h1>'
+    template: `
+        <h1>{{title}}</h1>
+        <h2>{{hero.name}} details!</h2>
+        <div><label>id: </label>{{hero.id}}</div>
+        <div><label>name: </label>{{hero.name}}</div>
+    `
 })
+
 export class AppComponent {
-    
+    title = 'Tour of Heroes';
+    hero : Hero = {
+        id : 1,
+        name: 'Windstorm'
+    };
 }
