078550d036fcad2f8d08b610885205e148c1bea0
xiaofuyesnew <xiaofuyesnew@qq.com>
2016-10-09 14:38:21 +0800

    many components

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,6 @@
+import { Hero } from './hero';
 import { Component } from '@angular/core';
-//Hero类
 
-export class Hero{
-    id : number;
-    name : string;
-}
 const HEROES: Hero[] = [
   { id: 11, name: 'Mr. Nice' },
   { id: 12, name: 'Narco' },
@@ -72,14 +68,6 @@ const HEROES: Hero[] = [
 `],
     template: `
         <h1>{{title}}</h1>
-        <div *ngIf="selectedHero">
-        <h2>{{selectedHero.name}} details!</h2>
-        <div><label>id: </label>{{selectedHero.id}}</div>
-        <div>
-            <label>name: </label>
-            <input type="text" [(ngModel)]="selectedHero.name" placeholder="name">
-        </div>
-        </div>
         <h2>My Heroes</h2>
         <ul class="heroes">
             <li *ngFor="let hero of heroes"
@@ -88,6 +76,7 @@ const HEROES: Hero[] = [
                 <span class="badge">{{hero.id}}</span>{{hero.name}}
             </li>
         </ul>
+        <my-hero-detail [hero]="selectedHero"></my-hero-detail>
     `
 })
 
--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -1,9 +1,21 @@
+import { Hero } from './hero';
 import {Component, Input} from '@angular/core';
 
 @Component({
-    selector: 'my-hero-detail'
+    selector: 'my-hero-detail',
+    template:`
+        <div *ngIf="hero">
+            <h2>{{hero.name}} details!</h2>
+            <div><label>id: </label>{{hero.id}}</div>
+            <div>
+                <label>name: </label>
+                <input type="text" [(ngModel)]="hero.name" placeholder="name">
+            </div>
+        </div>
+    `
 })
 
 export class HeroDetailComponent{
-
+    @Input()
+    hero: Hero;
 }
