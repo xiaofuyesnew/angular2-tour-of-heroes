d72c8574028862f48c9e1b9816b677f4e6e5d95d
xiaofuyesnew <xiaofuyesnew@qq.com>
2016-10-09 14:51:31 +0800

    ignore

--- app/hero-detail.component.js
+++ app/hero-detail.component.js
@@ -8,13 +8,19 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
+var hero_1 = require('./hero');
 var core_1 = require('@angular/core');
 var HeroDetailComponent = (function () {
     function HeroDetailComponent() {
     }
+    __decorate([
+        core_1.Input(), 
+        __metadata('design:type', hero_1.Hero)
+    ], HeroDetailComponent.prototype, "hero", void 0);
     HeroDetailComponent = __decorate([
         core_1.Component({
-            selector: 'my-hero-detail'
+            selector: 'my-hero-detail',
+            template: "\n        <div *ngIf=\"hero\">\n            <h2>{{hero.name}} details!</h2>\n            <div><label>id: </label>{{hero.id}}</div>\n            <div>\n                <label>name: </label>\n                <input type=\"text\" [(ngModel)]=\"hero.name\" placeholder=\"name\">\n            </div>\n        </div>\n    "
         }), 
         __metadata('design:paramtypes', [])
     ], HeroDetailComponent);
