4caa0782431f7d2e70bf867ac8a6210708920c39
xiaofuyesnew <xiaofuyesnew@qq.com>
2016-10-17 09:51:42 +0800

    service

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,18 +1,8 @@
 import { Hero } from './hero';
 import { Component } from '@angular/core';
+import { HeroService } from './hero.service';
 
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
+
 
 //装饰器
 @Component({
@@ -83,8 +73,18 @@ const HEROES: Hero[] = [
 export class AppComponent {
     title = 'Tour of Heroes';
     selectedHero: Hero;
-    heroes = HEROES;
+    heroes: Hero[];
+
     onSelect(hero: Hero): void {
         this.selectedHero = hero;
-    }
+    };
+
+    constructor(private heroService: HeroService) { };
+
+    providers: [HeroService];
+    
+    getHeroes(): void {
+        this.heroes = this.heroService.getHeroes();
+    };
+    
 }
