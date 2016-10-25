5d8c28b2c8dc35cae7ac52d7cfc38d05bfd47fba
xiaofuyesnew <xiaofuyesnew@qq.com>
2016-10-25 15:28:09 +0800

    router

--- app/app.component.ts
+++ app/app.component.ts
@@ -10,7 +10,7 @@ import { Component } from '@angular/core';
         </nav>
         <router-outlet></router-outlet>
     `,
-    styleUrls: ['app.component.css']
+    styleUrls: ['app/app.component.css']
 })
 
 export class AppComponent {
--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -7,7 +7,7 @@ import { HeroService } from './hero.service';
 @Component({
     moduleId: module.id,
     selector: 'my-dashboard',
-    template: 'dashboard.component.html',
+    templateUrl: 'dashboard.component.html',
     styleUrls: ['dashboard.component.css']
 })
 export class DashboardComponent implements OnInit{ 
